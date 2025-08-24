8a8c2342281922ff7ca1a672be43c06c9f7b5215
LUCASGOODPROGRAMMER <lucashubner2019@gmail.com>
2025-08-24 18:18:31 -0400

    server

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-    const jsonServer = require('json-server');
-    const server = jsonServer.create();
-    const router = jsonServer.router('db.json'); // Use seu arquivo JSON aqui
-    const middlewares = jsonServer.defaults();
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
-    server.use(middlewares);
-    server.use(router);
+server.use(middlewares);
+server.use(router);
 
-    const port = process.env.PORT || 3000; // O Render vai definir a porta
-
-    server.listen(port, () => {
-      console.log(`JSON Server is running on port ${port}`);
-    });
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log("JSON Server rodando na porta " + port);
+});
